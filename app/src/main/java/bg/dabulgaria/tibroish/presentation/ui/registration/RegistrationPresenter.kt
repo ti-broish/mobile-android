aca9df149b7c65beef10a94bab245e63361653dc
Antoan Angelov <antoan.angelov@gmail.com>
2021-05-11 22:38:01 +0100

    Fetch organizations in the registration screen

--- app/src/main/java/bg/dabulgaria/tibroish/presentation/ui/registration/RegistrationPresenter.kt
+++ app/src/main/java/bg/dabulgaria/tibroish/presentation/ui/registration/RegistrationPresenter.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import android.widget.CheckBox
 import bg.dabulgaria.tibroish.R
+import bg.dabulgaria.tibroish.domain.organisation.IOrganisationRepository
+import bg.dabulgaria.tibroish.domain.organisation.Organization
 import bg.dabulgaria.tibroish.presentation.base.BasePresenter
 import bg.dabulgaria.tibroish.presentation.base.IBasePresenter
 import bg.dabulgaria.tibroish.presentation.base.IDisposableHandler
@@ -11,43 +13,58 @@ import bg.dabulgaria.tibroish.presentation.main.IMainRouter
 import bg.dabulgaria.tibroish.presentation.ui.common.FormValidator
 import bg.dabulgaria.tibroish.utils.AssetReader
 import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 interface IRegistrationPresenter : IBasePresenter<IRegisterView> {
     fun getCountryCodes(context: Context, callback: (List<CountryCode>?) -> Unit)
 
-    fun getOrganizations(callback: (List<Organization>?) -> Unit)
+    fun getOrganizations(callback: (List<RegistrationOrganization>?) -> Unit)
 
     fun register(email: String, password: String)
 
     fun processRequiredField(text: String, callback: (Int) -> Unit): Boolean
 
     fun processEmailField(
-            email: String,
-            callback: (Int) -> Unit): Boolean
+        email: String,
+        callback: (Int) -> Unit
+    ): Boolean
 
     fun processPhoneNumberField(
-            telephone: String,
-            callback: (Int) -> Unit): Boolean
+        telephone: String,
+        callback: (Int) -> Unit
+    ): Boolean
 
     fun processEgnLastFourDigits(egn: String, callback: (Int) -> Unit): Boolean
 
-    fun processRepeatPassword(password: String, repeatPassword: String, callback: (Int) -> Unit): Boolean
+    fun processRepeatPassword(
+        password: String,
+        repeatPassword: String,
+        callback: (Int) -> Unit
+    ): Boolean
 
     fun processPassword(password: String, callback: (Int) -> Unit): Boolean
 
     fun processOrganization(organization: String, callback: (Int) -> Unit): Boolean
 
     fun processRequiredCheckbox(
-            checkBox: CheckBox, callback: (Int) -> Unit, errorCode: Int): Boolean
+        checkBox: CheckBox, callback: (Int) -> Unit, errorCode: Int
+    ): Boolean
 }
 
 class RegistrationPresenter @Inject constructor(
-        private val mainRouter: IMainRouter,
-        disposableHandler: IDisposableHandler) :
-        BasePresenter<IRegisterView>(disposableHandler), IRegistrationPresenter {
+    private val mainRouter: IMainRouter,
+    disposableHandler: IDisposableHandler
+) :
+    BasePresenter<IRegisterView>(disposableHandler), IRegistrationPresenter {
     private var registrationViewData: RegistrationViewData? = null
     private var formValidator: FormValidator = FormValidator()
+
+    @Inject
+    lateinit var organisationRepository: IOrganisationRepository
 
     override fun getCountryCodes(context: Context, callback: (List<CountryCode>?) -> Unit) {
         if (registrationViewData?.countryCodesData.isNullOrEmpty()) {
@@ -57,7 +74,7 @@ class RegistrationPresenter @Inject constructor(
         callback.invoke(registrationViewData?.countryCodesData)
     }
 
-    override fun getOrganizations(callback: (List<Organization>?) -> Unit) {
+    override fun getOrganizations(callback: (List<RegistrationOrganization>?) -> Unit) {
         if (registrationViewData?.organizationsData!!.isNullOrEmpty()) {
             loadOrganizationsAsync(callback);
             return
@@ -65,23 +82,32 @@ class RegistrationPresenter @Inject constructor(
         return callback(registrationViewData?.organizationsData)
     }
 
-    private fun loadOrganizationsAsync(callback: (List<Organization>?) -> Unit) {
-        // TODO: Change to a proper API call
-        val organizations = ArrayList(listOf(
-                Organization("Organization 1", "123"),
-                Organization("Hello 2", "124"),
-                Organization("Test 3", "125")))
+    private fun loadOrganizationsAsync(callback: (List<RegistrationOrganization>?) -> Unit) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val organizations: List<RegistrationOrganization> =
+                fetchOrganizations()
+                    .map { organization ->
+                        RegistrationOrganization(organization.name, organization.id)
+                    }
+                    .toList()
+            callback(organizations)
+            registrationViewData?.organizationsData?.clear()
+            registrationViewData?.organizationsData?.addAll(organizations)
+        }
+    }
 
-        callback(organizations)
-        registrationViewData?.organizationsData?.clear()
-        registrationViewData?.organizationsData?.addAll(organizations)
+    private suspend fun fetchOrganizations(): List<Organization> {
+        return withContext(Dispatchers.IO) {
+            organisationRepository.getOrganisations()
+        }
     }
 
     private fun loadCountryCodesAsync(context: Context, callback: (List<CountryCode>?) -> Unit) {
         val json: String? = AssetReader().loadJsonFromAsset(
 
-                "phone-codes.json",
-                context.applicationContext.assets)
+            "phone-codes.json",
+            context.applicationContext.assets
+        )
         val fromJson = Gson().fromJson(json, Countries::class.java)
         val countries = fromJson.countries
         callback.invoke(countries)
@@ -100,14 +126,16 @@ class RegistrationPresenter @Inject constructor(
     }
 
     override fun processEmailField(
-            email: String,
-            callback: (Int) -> Unit): Boolean {
+        email: String,
+        callback: (Int) -> Unit
+    ): Boolean {
         return formValidator.processEmailField(email, callback)
     }
 
     override fun processPhoneNumberField(
-            telephone: String,
-            callback: (Int) -> Unit): Boolean {
+        telephone: String,
+        callback: (Int) -> Unit
+    ): Boolean {
         if (!processRequiredField(telephone, callback)) {
             return false
         }
@@ -119,26 +147,34 @@ class RegistrationPresenter @Inject constructor(
             return false
         }
         if (!formValidator.processConditionalField(
-                        egn,
-                        predicate = {
-                            it.length == 4
-                        },
-                        callback = callback,
-                        errorResponse = R.string.egn_four_digits_required)) {
+                egn,
+                predicate = {
+                    it.length == 4
+                },
+                callback = callback,
+                errorResponse = R.string.egn_four_digits_required
+            )
+        ) {
             return false
         }
         return true
     }
 
-    override fun processRepeatPassword(password: String, repeatPassword: String, callback: (Int) -> Unit): Boolean {
+    override fun processRepeatPassword(
+        password: String,
+        repeatPassword: String,
+        callback: (Int) -> Unit
+    ): Boolean {
         if (!processRequiredField(repeatPassword, callback)) {
             return false
         }
         if (!formValidator.processConditionalField(
-                        password,
-                        predicate = { password == repeatPassword },
-                        callback = callback,
-                        errorResponse = R.string.passwords_do_not_match)) {
+                password,
+                predicate = { password == repeatPassword },
+                callback = callback,
+                errorResponse = R.string.passwords_do_not_match
+            )
+        ) {
             return false
         }
         return true
@@ -149,10 +185,12 @@ class RegistrationPresenter @Inject constructor(
             return false
         }
         if (!formValidator.processConditionalField(
-                        password,
-                        predicate = { it.length >= 6 },
-                        callback = callback,
-                        errorResponse = R.string.invalid_password_too_short)) {
+                password,
+                predicate = { it.length >= 6 },
+                callback = callback,
+                errorResponse = R.string.invalid_password_too_short
+            )
+        ) {
             return false
         }
         return true
@@ -167,21 +205,24 @@ class RegistrationPresenter @Inject constructor(
             return false
         }
         if (!formValidator.processConditionalField(
-                        organization,
-                        predicate = {
-                            registrationViewData?.organizationsData?.filter {
-                                it.name == organization
-                            }?.any() == true
-                        },
-                        callback = callback,
-                        errorResponse = R.string.invalid_organization)) {
+                organization,
+                predicate = {
+                    registrationViewData?.organizationsData?.filter {
+                        it.name == organization
+                    }?.any() == true
+                },
+                callback = callback,
+                errorResponse = R.string.invalid_organization
+            )
+        ) {
             return false
         }
         return true
     }
 
     override fun processRequiredCheckbox(
-            checkBox: CheckBox, callback: (Int) -> Unit, errorCode: Int): Boolean {
+        checkBox: CheckBox, callback: (Int) -> Unit, errorCode: Int
+    ): Boolean {
         if (!checkBox.isChecked) {
             callback(errorCode)
             return false
@@ -191,9 +232,10 @@ class RegistrationPresenter @Inject constructor(
 
     override fun onRestoreData(bundle: Bundle?) {
         registrationViewData = bundle?.getParcelable(RegistrationConstants.VIEW_DATA_KEY)
-                ?: RegistrationViewData(
-                        /* countryCodesData= */ mutableListOf(),
-                        /* organizationsData= */mutableListOf())
+            ?: RegistrationViewData(
+                /* countryCodesData= */ mutableListOf(),
+                /* organizationsData= */mutableListOf()
+            )
     }
 
     override fun onSaveData(outState: Bundle) {
@@ -203,8 +245,9 @@ class RegistrationPresenter @Inject constructor(
     override fun loadData() {
         if (registrationViewData == null) {
             registrationViewData = RegistrationViewData(
-                    /* countryCodesData= */ mutableListOf(),
-                    /* organizationsData= */mutableListOf())
+                /* countryCodesData= */ mutableListOf(),
+                /* organizationsData= */mutableListOf()
+            )
         }
     }
 }
