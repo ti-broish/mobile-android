55657259e2f40583146240a15d4220511790cbf6
Antoan Angelov <antoan.angelov@gmail.com>
2021-05-18 22:23:01 +0100

    Add registration API calls

--- app/src/main/java/bg/dabulgaria/tibroish/persistence/remote/api/TiBroishApiController.kt
+++ app/src/main/java/bg/dabulgaria/tibroish/persistence/remote/api/TiBroishApiController.kt
@@ -2,10 +2,14 @@ package bg.dabulgaria.tibroish.persistence.remote.api
 
 import bg.dabulgaria.tibroish.domain.organisation.Organization
 import bg.dabulgaria.tibroish.domain.locations.Country
+import bg.dabulgaria.tibroish.domain.user.User
+import bg.dabulgaria.tibroish.domain.user.UserS
 import retrofit2.Call
+import retrofit2.http.Body
 
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface TiBroishApiController {
 
@@ -14,4 +18,8 @@ interface TiBroishApiController {
 
     @GET("/countries")
     fun getCountries(@Header(ApiHeader.Authorization) authorization:String): Call<List<Country>>
+
+    @POST("/users")
+    fun createUser(@Header(ApiHeader.Authorization) authorization: String, @Body userData: User)
+            :Call<Any>
 }
--- app/src/main/java/bg/dabulgaria/tibroish/presentation/ui/registration/RegistrationOrganization.kt
+++
@@ -1,8 +0,0 @@
-package bg.dabulgaria.tibroish.presentation.ui.registration
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-import java.math.BigDecimal
-
-@Parcelize
-data class RegistrationOrganization (val name: String, val id: BigDecimal) : Parcelable
