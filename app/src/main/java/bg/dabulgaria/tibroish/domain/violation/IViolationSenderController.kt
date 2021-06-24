39e848ccd4298d685fbcbea74d34301dcb711948
Antoan Angelov <antoan.angelov@gmail.com>
2021-06-24 21:07:07 +0100

    Use a foreground service to upload protocols and signals

--- app/src/main/java/bg/dabulgaria/tibroish/domain/violation/IViolationSenderController.kt
+++ app/src/main/java/bg/dabulgaria/tibroish/domain/violation/IViolationSenderController.kt
@@ -1,5 +1,10 @@
 package bg.dabulgaria.tibroish.domain.violation
 
+import android.content.Context
+import android.content.Intent
+
 interface IViolationSenderController {
-    fun upload(metadata: ViolationMetadata)
+    fun upload(metadata: ViolationMetadata): String
+
+    fun getIntent(context: Context, violationServerId: String): Intent
 }
